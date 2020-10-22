7c0a7de473c9a23001446d90aa9a0a2af7576f3a
A00269349 <a00269349@student.ait.ie>
2020-10-22 11:41:19 +0100

    PG: Added Decorations and Enemies Spawns

---
+++ Assets/Code/Procedural Generation/SceneReloader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloader : MonoBehaviour
+{
+    public Animator transisition;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LoadNextLevel();
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    private IEnumerator LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+        
+        yield return new WaitForSeconds(1); transisition.SetTrigger("Start");
+
+    }
+}
--- Assets/Prefabs/SixDotsPG/ColorToGameObject.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public class ColorToGameObject { //this is a class, rather than a struct, because structs are not serializable in Unity
-	public Color color;
-	public GameObject prefab;
-}
