e19ad970c13ec06cfbcef25375083bf767bdf816
A00271688 <71632811+A00271688@users.noreply.github.com>
2020-10-25 01:34:32 +0100

    Adam: Code for Bullet no animation yet
    No bullet animation yet will push again as soon as i get it working

---
+++ Assets/Code/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 shootDir;
+    public void Setup(Vector3 shootDir)
+    {
+        this.shootDir = shootDir;
+        transform.eulerAngles = new Vector3(0, 0, GetAngelFromVectorFloat(shootDir));
+        Destroy(gameObject, 5f);
+    }
+
+    public static float GetAngelFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+    private void Update()
+    {
+        float moveSpeed = 100f;
+        transform.position += shootDir * moveSpeed * Time.deltaTime;
+    }
+}
---
+++ Assets/Code/PlayerShootProjectiles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootProjectiles : MonoBehaviour
+{
+    [SerializeField] private Transform pfbullet;
+
+    private void Awake()
+    {
+        GetComponent<PlayerAimWeapon>().OnShoot += PlayerShootProjectiles_OnShoot;
+    }
+
+    private void PlayerShootProjectiles_OnShoot(object sender, PlayerAimWeapon.OnShootEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PlayerShootProjectlies_OnShoot(object sender, PlayerAimWeapon.OnShootEventArgs e)
+    {
+        //shoot
+       Transform bulletTransform =  Instantiate(pfbullet, e.gunEndPointPosition, Quaternion.identity);
+
+        Vector3 shootDir = (e.shootPosition - e.gunEndPointPosition).normalized;
+        bulletTransform.GetComponent<Bullet>().Setup(shootDir);
+    }
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        target target = collider.GetComponent<Target>();
+        if(target != null)
+        {
+            target.Damage();
+            Destroy(gameObject);
+        }
+    }*/
+    // This is the code for bullet collision (replace "target" with enemy)
+}
