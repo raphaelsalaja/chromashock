c4e03c87098a4631f9903a5d63bd4dccd938bad8
A00270065 <a00270065@student.ait.ie>
2020-10-30 17:52:20 +0000

    Gearoid

---
+++ Assets/Scenes/Main Menu Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
